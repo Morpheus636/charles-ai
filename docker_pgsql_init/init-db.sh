b12b6359b73aae4f992ec6e1df4dfe1731936d34
Josh Levin <morpheus636@morpheus636.com>
2023-03-25 22:24:00 -0400

    repo updates

--- docker_pgsql_init/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER localdev WITH PASSWORD 'localdev_secret';
-    CREATE DATABASE replaceme;
-    GRANT ALL PRIVILEGES ON DATABASE replaceme TO localdev;
-EOSQL
