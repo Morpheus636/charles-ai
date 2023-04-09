f43aa41f8b1c513c337e730ddbdf7a9415ba9824
Josh Levin <morpheus636@morpheus636.com>
2023-04-09 23:29:48 +0000

    added timezone to datetime

--- src/charles_ai/plugin_api/plugin_modules/plugin_datetime.py
+++ src/charles_ai/plugin_api/plugin_modules/plugin_datetime.py
@@ -1,8 +1,20 @@
 import datetime
 
 
-spec = {"plugin": "datetime", "description": "Fetches the current date and time.", "args": {}}
+spec = {
+    "plugin": "datetime",
+    "description": "Fetches the current date and time.",
+    "args": {
+        "timezone_offset": "Signed integer representing the conversion from UTC to the requested timezone."
+    },
+}
 
 
 def run(**kwargs):
-    return f"Current date and time: {datetime.datetime.now().strftime('%I:%M %p on %B %d, %Y')}"
+    tz_offset = kwargs.get("timezone_offset")
+    if not tz_offset:
+        tz_offset = 0
+    tzinfo = datetime.timezone(datetime.timedelta(hours=int(tz_offset)))
+    return (
+        f"Current date and time: {datetime.datetime.now(tzinfo).strftime('%I:%M %p on %B %d, %Y')}"
+    )
