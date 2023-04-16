074c09ae0d21358518cfd78857a2380e07f95bf5
Josh Levin <morpheus636@morpheus636.com>
2023-04-16 00:53:51 +0000

    improved prompt

--- src/charles_ai/plugin_api/plugin_modules/plugin_wikipedia.py
+++ src/charles_ai/plugin_api/plugin_modules/plugin_wikipedia.py
@@ -3,7 +3,7 @@ import wikipediaapi
 
 spec = {
     "plugin": "wikipedia",
-    "description": "Gets the summary of a wikipedia page. Use this plugin when asked for more information than you know, or if the user asks you to look something up.",
+    "description": "Gets the summary of a wikipedia page. Use this plugin when you need more information than you know, or if the user asks you to look something up.",
     "args": {
         "page_name": "The title of the wikipedia page to get. Try guessing this based on context before explicitly asking the user for it."
     },
